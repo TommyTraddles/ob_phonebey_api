a02e4ccea8d85a04a123e58ccdd14c8481202f3d
Tommy Traddles <jandresnova@gmail.com>
2022-08-28 10:18:43 +0200

    chore: adding get-all base test

--- src/test/api-availability.test.js
+++ src/test/api-availability.test.js
@@ -1,14 +1,46 @@
 const { app } = require('../index')
 const api = require('supertest')(app)
 
-describe('GET / [Endpoint availability]', () => {
+describe('GET /', () => {
   test('API should be operative', async () => {
     const expected = {
       statusCode: 200,
       success: true,
     }
-    const response = await api.get('/')
-    expect(response.status).toEqual(expected.statusCode)
-    expect(JSON.parse(response.text).success).toEqual(expected.success)
+    const query = await api.get('/')
+    const response = JSON.parse(query.text)
+    expect(query.status).toEqual(expected.statusCode)
+    expect(response.success).toEqual(expected.success)
+  })
+})
+
+describe('GET /get-all', () => {
+  const endpoint = '/get-all'
+
+  test('Endpoint should be operative', async () => {
+    const expected = {
+      statusCode: 200,
+      success: true,
+    }
+    const query = await api.get(endpoint)
+    const response = JSON.parse(query.text)
+    expect(query.status).toEqual(expected.statusCode)
+    expect(response.success).toEqual(expected.success)
+  })
+
+  test('Endpoint should return all phones', async () => {
+    const query = await api.get(endpoint)
+    const response = JSON.parse(query.text)
+
+    response.data.forEach((phone) => {
+      expect(phone.id).toBeDefined()
+      expect(phone.new).toBeDefined()
+      expect(phone.name).toBeDefined()
+      expect(phone.brand).toBeDefined()
+      expect(phone.price).toBeDefined()
+      expect(phone.color).toBeDefined()
+      expect(phone.images).toBeDefined()
+      expect(phone.bestseller).toBeDefined()
+    })
   })
 })
